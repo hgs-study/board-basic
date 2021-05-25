de0a20ed37dfe71dc6cc2e1b3077cb2863b1265b
hgs-study <hgstudy_@naver.com>
2021-05-25 22:26:06 +0900

    [feature/7] [Post] 게시물 조회, 수정, 삭제

--- src/main/java/com/boardbasic/business/board/domain/post/application/PostService.java
+++ src/main/java/com/boardbasic/business/board/domain/post/application/PostService.java
@@ -1,16 +1,43 @@
 package com.boardbasic.business.board.domain.post.application;
 
+import com.boardbasic.business.board.domain.post.entity.Post;
 import com.boardbasic.business.board.domain.post.form.PostForm.*;
+import com.boardbasic.common.error.code.ErrorCode;
+import com.boardbasic.common.error.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostService {
 
     private final PostRepository postRepository;
 
-    public void save(Request.Add add){
-        postRepository.save(Request.Add.toEntity(add));
+    @Transactional
+    public void save(Post post){
+        postRepository.save(post);
+    }
+
+    public List<Post> findPosts(){
+        return postRepository.findAll();
+    }
+
+    public Post findPost(Long id){
+        return postRepository.findById(id)
+                                .orElseThrow(()-> new BusinessException(ErrorCode.NOT_FOUND_POST));
+    }
+
+    @Transactional
+    public void modifyPost(Post post){
+        postRepository.save(post);
+    }
+
+    @Transactional
+    public void deletePost(Post post){
+        postRepository.delete(post);
     }
 }
--- src/main/java/com/boardbasic/business/board/domain/post/form/PostForm.java
+++ src/main/java/com/boardbasic/business/board/domain/post/form/PostForm.java
@@ -1,6 +1,7 @@
 package com.boardbasic.business.board.domain.post.form;
 
 import com.boardbasic.business.board.domain.post.entity.Post;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,8 +13,41 @@ public class PostForm {
             private String title;
             private String content;
 
-            public static Post toEntity(Add add){
-                return new Post(add.title, add.content);
+            public Post toEntity(){
+                return new Post(title, content);
+            }
+        }
+
+        @Getter
+        @NoArgsConstructor
+        public static class Modify{
+            private Long id;
+            private String title;
+            private String content;
+
+        }
+    }
+
+    public static class Response{
+
+        @Getter
+        public static class Find{
+            private Long id;
+            private String title;
+            private String content;
+
+            private Find(Long id, String title, String content){
+                this.id = id;
+                this.title = title;
+                this.content = content;
+            }
+
+            public static Find of(Post post){
+                return new Find(
+                            post.getId(),
+                            post.getTitle(),
+                            post.getContent()
+                        );
             }
         }
     }
